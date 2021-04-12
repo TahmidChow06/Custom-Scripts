cca46dee8f7fd1155e33fc2f70ab6e9eb08b1a08
TahmidChow06 <76938822+TahmidChow06@users.noreply.github.com>
2021-04-12 16:32:07 +0600

    Update DEEP-WEB-LINKS.sh

--- DEEP-WEB-LINKS.sh
+++ DEEP-WEB-LINKS.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-promptmMssage="LINKS" # Message should of one word
-menu="rofi -dmenu -p $promptmMssage -theme ~/Downloads/aur/rofi-collection/minimal/minimal.rasi"
-linkType=$(echo -e 'Search Engine\nChat Rooms\nForums\nMarkets' | $menu)
+promptmMssage="LINKS:" # Message should of one word
+menu="rofi -dmenu -p $promptmMssage -i -theme ~/Downloads/aur/rofi-collection/minimal/minimal.rasi"
+linkType=$(echo -e 'Search Engine\nChat Rooms\nForums\nMarkets\nMedia' | $menu)
 
 if [ "$linkType" == "Search Engine" ] # Search Engine
 then
@@ -19,19 +19,19 @@ then
 elif [ "$linkType" == "Chat Rooms" ] # Chat Rooms
 then
   search=$(echo -e "John Doe\nBlack Hat Chat\nAbleonion\nThe End Chat\nDaniel's Chat" | $menu)
-    if [ "$linkType" = "John Doe" ]
+    if [ "$search" = "John Doe" ]
     then
       echo 'tetatl6umgbmtv27.onion' | xclip -selection clipboard
-    elif [ "$linkType" = "Black Hat Chat" ]
+    elif [ "$search" = "Black Hat Chat" ]
     then
       echo 'blkhatjxlrvc5aevqzz5t6kxldayog6jlx5h7glnu44euzongl4fh5ad.onion' | xclip -selection clipboard
-    elif [ "$linkType" = "Ableonion" ]
+    elif [ "$search" = "Ableonion" ]
     then
       echo 'notbumpz34bgbz4yfdigxvd6vzwtxc3zpt5imukgl6bvip2nikdmdaad.onion' | xclip -selection clipboard
-    elif [ "$linkType" = "The End Chat" ]
+    elif [ "$search" = "The End Chat" ]
     then
       echo 'theendgtso35ir6ngdtyhgtjhhbbprmkzl74gt5nyeu3ocr34sfa67yd.onion' | xclip -selection clipboard
-    elif [ "$linkType" = "Daniel's Chat" ]
+    elif [ "$search" = "Daniel's Chat" ]
     then
       echo 'danschat356lctri3zavzh6fbxg2a7lo6z3etgkctzzpspewu7zdsaqd.onion' | xclip -selection clipboard
     fi
@@ -41,11 +41,18 @@ then
     if [ "$linkType" = "Truth Board" ]
     then
       echo 'k5aintllrufq23khjnmmfli6uxioboe3ylcao7k72mk2bgvwqb5ek4ad.onion' | xclip -selection clipboard
-    elif [ "$linkType" = "Alien Forum" ]
+    elif [ "$search" = "Alien Forum" ]
     then
       echo 'alienswjstz7elk6v3x7trrbqmtkcocipqregx7kwzzu44rvs7kaftid.onion' | xclip -selection clipboard
-    elif [ "$linkType" = "Cavetor" ]
+    elif [ "$search" = "Cavetor" ]
     then
-      echo 'http://cavetord6bosm3sl.onion/forum' | xclip -selection clipboard
+      echo 'cavetord6bosm3sl.onion/forum' | xclip -selection clipboard
+    fi
+elif [ "$linkType" == "Media" ] # Media
+then
+  search=$(echo -e 'Onion Tube' | $menu)
+    if [ "$search" = "Onion Tube" ]
+    then
+      echo 'tubemwive7gh35yolhtbuvxc52xncrrurk52u5eblil7cqx3krndvqqd.onion' | xclip -selection clipboard
     fi
 fi
